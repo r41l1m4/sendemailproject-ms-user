96b159eaa9731af70eabde62ff166c77e4fcff9e
r41l1m4 <rai.lima17@gmail.com>
2023-10-23 15:36:05 -0300

    refactor save method on UserService

--- src/main/java/dev/ironia/ms/user/services/UserService.java
+++ src/main/java/dev/ironia/ms/user/services/UserService.java
@@ -1,6 +1,7 @@
 package dev.ironia.ms.user.services;
 
 import dev.ironia.ms.user.models.UserModel;
+import dev.ironia.ms.user.producers.UserProducer;
 import dev.ironia.ms.user.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -9,13 +10,17 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserProducer userProducer;
 
-    UserService(UserRepository userRepository) {
+    UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
     public UserModel save(UserModel userModel) {
-        return userRepository.save(userModel);
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
     }
 }
